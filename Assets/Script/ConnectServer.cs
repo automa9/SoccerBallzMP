92b5dccfd91c7a84591de27ca5e62934fd4c3d33
chonghonglei <chonghonglei1999@gmail.com>
2023-07-08 11:13:10 +0800

    loading bar

--- Assets/Script/ConnectServer.cs
+++ Assets/Script/ConnectServer.cs
@@ -27,18 +27,44 @@ public class ConnectServer : MonoBehaviourPunCallbacks
     public void ConnectMultiplayer()
     {
         loadingBar.SetActive(true); // Show the loading bar or sprite when connecting
+        loadingPanel.SetActive(true);
+        StartCoroutine(SimulateConnectionProgress());
+    }
+
+    private IEnumerator SimulateConnectionProgress()
+    {
+        float progress = 0f;
+        while (progress < 0.5f)
+        {
+            progress += Time.deltaTime; // Adjust the speed of progress increment
+
+            // Clamp the progress value between 0 and 1
+            progress = Mathf.Clamp01(progress);
+
+            // Update the loading bar with the simulated progress
+            loadingBarController.UpdateLoadingBar(progress);
+
+            yield return null;
+        }
+
+        // Connection process completed, proceed to the next step
+        OnConnectionProgressComplete();
+    }
+
+    private void OnConnectionProgressComplete()
+    {
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
-        loadingBarController.UpdateLoadingBar(0.5f); // Update the loading bar progress
+        loadingBarController.UpdateLoadingBar(0.8f); // Update the loading bar progress
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {   
-        loadingPanel.SetActive(true);
+        
         loadingBarController.UpdateLoadingBar(1f); // Update the loading bar progress
         SceneManager.LoadScene(lobbySceneName);
     }
